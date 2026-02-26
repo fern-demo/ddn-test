18484404e7f70ca6f230b180c2836526ee319d98
achaudhary-ddn <achaudhary@ddn.com>
2026-02-26 18:09:46 +0530

    Fern CLI Upgrade

---
+++ fern/components/CustomFooter.tsx
@@ -0,0 +1,9 @@
+export default function CustomFooter() {
+    return (
+        <footer className="w-full py-8 px-6 bg-gray-100 dark:bg-gray-900">
+            <div className="max-w-7xl mx-auto">
+                Plant Store API Documentation. All rights reserved.
+            </div>
+        </footer>
+    );
+}
