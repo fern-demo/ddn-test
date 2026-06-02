381bfeaa77fd8cd9fd560188e7800b6cc2ec227b
achaudhary-ddn <achaudhary@ddn.com>
2026-06-02 19:07:14 +0530

    Updated styling for the sandbox site

--- fern/components/CustomFooter.tsx
+++ fern/components/CustomFooter.tsx
@@ -1,8 +1,10 @@
 export default function CustomFooter() {
     return (
         <footer className="w-full py-8 px-6 bg-gray-100 dark:bg-gray-900">
-            <div className="max-w-7xl mx-auto">
+            <div className="max-w-7xl mx-auto text-center text-sm text-gray-600 dark:text-gray-400">
                 Plant Store API Documentation. All rights reserved.
+                <br/>
+                <a href="/copyright-information">Copyright Information</a>
             </div>
         </footer>
     );
